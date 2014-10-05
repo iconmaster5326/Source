9fe3ef9584c3e58c976122757bd6310c9a67a2cc
Iconmaster <crazybonesj@yahoo.com>
2014-10-04 20:26:18 -0400

    Enhanced CLA options

--- src/com/iconmaster/source/Source.java
+++ src/com/iconmaster/source/Source.java
@@ -4,9 +4,13 @@ import com.iconmaster.source.element.Element;
 import com.iconmaster.source.exception.SourceException;
 import com.iconmaster.source.parse.Parser;
 import com.iconmaster.source.tokenize.Tokenizer;
+import com.iconmaster.source.util.CLAHelper;
+import com.iconmaster.source.util.CLAHelper.CLA;
 import com.iconmaster.source.util.Debug;
 import com.iconmaster.source.xml.ElementXML;
 import com.iconmaster.source.xml.XMLHelper;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -20,31 +24,63 @@ import org.w3c.dom.Document;
 public class Source {
 
 	public static void main(String[] args) {
+		CLA cla = CLAHelper.getArgs(args);
+		if (cla.containsKey("d")) {
+			Debug.debugMode = true;
+		}
+		if (cla.containsKey("i")) {
+			interactivePrompt();
+		}
+		if (cla.containsKey("code")) {
+			printInput(cla.get("code"));
+		}
+		if (cla.unmatched.length == 0) {
+			return;
+		} else {
+			cla.put("f",cla.unmatched[0]);
+		}
+		if (cla.containsKey("f")) {
+			String input = cla.get("f");
+			File file = new File(input);
+			if (!file.canRead()) {
+				System.out.println("ERROR: file "+input+" can't be read from!");
+				return;
+			}
+			try {
+				input = "";
+				Scanner fileScanner = new Scanner(new File(cla.get("f")));
+				while (fileScanner.hasNext()){
+				   input+="\n"+fileScanner.next();
+				}
+				
+				
+			} catch (FileNotFoundException ex) {
+				System.out.println("ERROR: file "+input+" can't be read from!");
+				return;
+			}
+			printInput(input);
+		}
+	}
+	
+	public static void interactivePrompt() {
 		Scanner kbd = new Scanner(System.in);
 		while (true) {
 			ArrayList<Element> a = null;
-			try {
-				String input;
-				if (args!=null && args.length>0 && args[0]!= null && args[0].equals("-d")) {
-					Debug.debugMode = true;
-					input = (args.length<2 || args[1]==null)?"":args[1];
-				} else {
-					System.out.print("Enter an expression: ");
-					input = kbd.next();
-				}
-				a = Tokenizer.tokenize(input);
-				a = Parser.parse(a);
-			} catch (SourceException ex) {
-				Logger.getLogger(Source.class.getName()).log(Level.SEVERE, null, ex);
-			}
-
+			System.out.print("Enter an expression: ");
+			printInput(kbd.next());
+		}
+	}
+	
+	public static void printInput(String input) {
+		try {
+			ArrayList<Element> a = Parser.parse(Tokenizer.tokenize(input));
 			System.out.println(a);
+
 			Document doc = XMLHelper.blankDoc();
 			ElementXML.toXML(doc, XMLHelper.addTag(doc, "parse_result", ""), a);
 			System.out.println(XMLHelper.toString(doc));
-			if (Debug.debugMode) {
-				break;
-			}
+		} catch (SourceException ex) {
+			Logger.getLogger(Source.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 	
---
+++ src/com/iconmaster/source/util/CLAHelper.java
@@ -0,0 +1,40 @@
+package com.iconmaster.source.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author iconmaster
+ */
+public class CLAHelper {
+	public static class CLA extends HashMap<String,String> {
+		public String[] unmatched;
+	}
+	
+	public static CLA getArgs(String[] input) {
+		CLA cla = new CLA();
+		String flag = null;
+		ArrayList<String> a = new ArrayList<>();
+		for (String s : input) {
+			if (s.startsWith("-")) {
+				if (flag!=null) {
+					cla.put(flag, "");
+				}
+				flag = s.substring(1);
+			} else {
+				if (flag==null) {
+					a.add(s);
+					flag = null;
+				} else {
+					cla.put(flag, s);
+				}
+			}
+		}
+		if (flag!=null) {
+			cla.put(flag, "");
+		}
+		cla.unmatched = a.toArray(input);
+		return cla;
+	}
+}
