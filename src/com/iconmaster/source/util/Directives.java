fa45e72296331449bfb55b5978b4d94f57be1162
Iconmaster <crazybonesj@yahoo.com>
2014-10-30 18:44:49 -0400

    Antidirecitves!

--- src/com/iconmaster/source/util/Directives.java
+++ src/com/iconmaster/source/util/Directives.java
@@ -8,7 +8,20 @@ import java.util.ArrayList;
  */
 public class Directives {
 	public static boolean has(IDirectable obj, String dir) {
-		return obj.getDirectives().contains(dir);
+		boolean neg = false;
+		if (dir.startsWith("!")) {
+			dir = dir.substring(1);
+			neg = true;
+		}
+		int count = 0;
+		for (String dir2 : obj.getDirectives()) {
+			if (dir2.startsWith(dir)) {
+				count++;
+			} else if (dir2.startsWith("!"+dir)) {
+				count--;
+			}
+		}
+		return neg?count<=0:count>0;
 	}
 	
 	public static ArrayList<String> getAll(IDirectable obj) {
