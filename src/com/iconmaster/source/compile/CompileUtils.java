a467f61acd1da55606c1c03249e22dc85076d040
Iconmaster <crazybonesj@yahoo.com>
2014-11-19 19:07:20 -0500

    Added goto inlining for upward for loops

--- src/com/iconmaster/source/compile/CompileUtils.java
+++ src/com/iconmaster/source/compile/CompileUtils.java
@@ -13,21 +13,24 @@ import java.util.ArrayList;
  * @author iconmaster
  */
 public class CompileUtils {
+
 	public static interface CodeTransformer {
+
 		public ArrayList<Operation> transform(SourcePackage pkg, Object workingOn, ArrayList<Operation> code);
 	}
-	
+
 	public static abstract class FunctionCallTransformer implements CodeTransformer {
+
 		public Function function;
 
 		public FunctionCallTransformer(Function function) {
 			this.function = function;
 		}
-		
+
 		@Override
 		public ArrayList<Operation> transform(SourcePackage pkg, Object workingOn, ArrayList<Operation> code) {
 			ArrayList<Operation> a = new ArrayList<>();
-			for (int i=0;i<code.size();i++) {
+			for (int i = 0; i < code.size(); i++) {
 				Operation op = code.get(i);
 				if (op.op == OpType.CALL && op.args[1].equals(function.getFullName())) {
 					a.addAll(onCall(pkg, workingOn, code, op));
@@ -37,18 +40,16 @@ public class CompileUtils {
 			}
 			return a;
 		}
-		
+
 		public abstract ArrayList<Operation> onCall(SourcePackage pkg, Object workingOn, ArrayList<Operation> code, Operation op);
 	}
-	
-	public static final CodeTransformer gotoReplacer = (pkg, workingOn, code) -> replaceWithGotos(pkg, code);
-	
+
 	public static final CodeTransformer forEachReplacer = (pkg, workingOn, code) -> {
 		ArrayList<Operation> a = new ArrayList<>();
-		for (int i=0;i<code.size();i++) {
+		for (int i = 0; i < code.size(); i++) {
 			Operation op = code.get(i);
 
-			if (op.op==OpType.FORE) {
+			if (op.op == OpType.FORE) {
 				String temp = pkg.nameProvider.getTempName();
 				String temp2 = pkg.nameProvider.getTempName();
 				a.add(new Operation(OpType.CALL, TypeDef.INT, op.range, temp2, "list.size", op.args[0]));
@@ -62,7 +63,7 @@ public class CompileUtils {
 		}
 		return a;
 	};
-	
+
 	public static final CodeTransformer iteratorReplacer = (pkg, work, code) -> {
 		ArrayList<Operation> a = new ArrayList<>();
 		ArrayList<Operation> old = a;
@@ -70,10 +71,10 @@ public class CompileUtils {
 		Operation forOp = null;
 		int depth = 0;
 		boolean found = false;
-		
-		for (int i=0;i<code.size();i++) {
+
+		for (int i = 0; i < code.size(); i++) {
 			Operation op = code.get(i);
-			
+
 			if (op.op == OpType.FORC) {
 				found = true;
 				a = new ArrayList<>();
@@ -83,16 +84,16 @@ public class CompileUtils {
 			} else if (op.op == OpType.ENDB) {
 				if (found) {
 					depth--;
-					if (depth==0) {
+					if (depth == 0) {
 						ArrayList<Operation> block = a;
 						a = old;
 						ArrayList<Operation> trans = new ArrayList<>();
-						for (int k=1;k<1+iter.getArguments().size();k++) {
-							trans.add(new Operation(OpType.MOV, iter.getArguments().get(k-1).getName(), forOp.args[k]));
+						for (int k = 1; k < 1 + iter.getArguments().size(); k++) {
+							trans.add(new Operation(OpType.MOV, iter.getArguments().get(k - 1).getName(), forOp.args[k]));
 						}
 						for (Operation op2 : iter.getCode()) {
-							if (op2.op==OpType.RET) {
-								int ii = 1+iter.getArguments().size();
+							if (op2.op == OpType.RET) {
+								int ii = 1 + iter.getArguments().size();
 								for (String arg : op2.args) {
 									trans.add(new Operation(OpType.MOV, forOp.args[ii], arg));
 									ii++;
@@ -121,17 +122,23 @@ public class CompileUtils {
 		}
 		return a;
 	};
-	
+
+	public static final CodeTransformer gotoReplacer = (pkg, workingOn, code) -> {
+		code = iteratorReplacer.transform(pkg, workingOn, code);
+		code = forEachReplacer.transform(pkg, workingOn, code);
+		return replaceWithGotos(pkg, code);
+	};
+
 	public static void transform(SourcePackage pkg, CodeTransformer ct) {
 		for (Function f : pkg.getFunctionsAndIterators()) {
-			if (f.getCode()!=null) {
+			if (f.getCode() != null) {
 				ArrayList<Operation> code = ct.transform(pkg, f, f.getCode());
 				f.setCompiled(code);
 			}
 		}
-		
+
 		for (Field f : pkg.getFields()) {
-			if (f.getValue()!=null) {
+			if (f.getValue() != null) {
 				ArrayList<Operation> code = ct.transform(pkg, f, f.getValue());
 				Expression e = new Expression();
 				e.addAll(code);
@@ -139,36 +146,41 @@ public class CompileUtils {
 			}
 		}
 	}
-	
+
 	public static ArrayList<Operation> replaceWithGotos(SourcePackage pkg, ArrayList<Operation> code) {
 		ArrayList<Operation> a = new ArrayList<>();
 		ArrayList<Operation> old = a;
-		
+
 		ArrayList<Operation> doBlock = null;
-		
+
 		String begin = null;
 		String end = null;
-		
+
 		boolean isWhile = false;
 		ArrayList<Operation> whileBlock = null;
-		
+
 		boolean isRep = false;
 		String repCond = null;
 		ArrayList<Operation> repBlock = null;
-		
+
 		boolean isIf = false;
 		ArrayList<Operation> ifBlock = null;
 		ArrayList<Operation> elseBlock = null;
 		String elseLabel = null;
 		String endLabel = null;
-		
+
+		boolean isFor = false;
+		ArrayList<Operation> forBlock = null;
+		String forLabel = null;
+		Operation forOp = null;
+
 		int depth = 0;
-		
-		for (int i=0;i<code.size();i++) {
+
+		for (int i = 0; i < code.size(); i++) {
 			Operation op = code.get(i);
 			switch (op.op) {
 				case DO:
-					if (isWhile || isRep || isIf) {
+					if (isWhile || isRep || isIf || isFor) {
 						depth++;
 						a.add(op);
 						break;
@@ -179,7 +191,7 @@ public class CompileUtils {
 					a = doBlock;
 					break;
 				case WHILE:
-					if (isWhile || isRep || isIf) {
+					if (isWhile || isRep || isIf || isFor) {
 						a.add(op);
 						break;
 					}
@@ -191,7 +203,7 @@ public class CompileUtils {
 					a = whileBlock;
 					break;
 				case REP:
-					if (isWhile || isRep || isIf) {
+					if (isWhile || isRep || isIf || isFor) {
 						a.add(op);
 						break;
 					}
@@ -201,7 +213,7 @@ public class CompileUtils {
 					a = repBlock;
 					break;
 				case ENDB:
-					if (depth>0) {
+					if (depth > 0) {
 						depth--;
 						a.add(op);
 						break;
@@ -221,7 +233,7 @@ public class CompileUtils {
 						isRep = false;
 					}
 					if (isIf) {
-						if (elseBlock!=null) {
+						if (elseBlock != null) {
 							old.addAll(replaceWithGotos(pkg, elseBlock));
 						} else {
 							old.addAll(replaceWithGotos(pkg, ifBlock));
@@ -231,9 +243,18 @@ public class CompileUtils {
 						a = old;
 						isIf = false;
 					}
+					if (isFor) {
+						old.addAll(replaceWithGotos(pkg, forBlock));
+						old.add(new Operation(OpType.ADD, forOp.type, null, forOp.args[0], forOp.args[0], forOp.args.length<4?"1":forOp.args[4]));
+						String temp = pkg.nameProvider.getTempName();
+						old.add(new Operation(OpType.GT, TypeDef.BOOLEAN, null, temp, forOp.args[0], forOp.args[3]));
+						old.add(new Operation(OpType.GOTOF, temp, forLabel));
+						a = old;
+						isFor = false;
+					}
 					break;
 				case IF:
-					if (isWhile || isRep || isIf) {
+					if (isWhile || isRep || isIf || isFor) {
 						a.add(op);
 						break;
 					}
@@ -247,8 +268,8 @@ public class CompileUtils {
 					a = ifBlock;
 					break;
 				case ELSE:
-					if (isWhile || isRep || isIf) {
-						if (depth==0) {
+					if (isWhile || isRep || isIf || isFor) {
+						if (depth == 0) {
 							old.addAll(replaceWithGotos(pkg, ifBlock));
 							old.add(new Operation(OpType.GOTO, op.range, endLabel));
 							old.add(new Operation(OpType.LABEL, op.range, elseLabel));
@@ -260,13 +281,27 @@ public class CompileUtils {
 						break;
 					}
 					break;
+				case FORR:
+					if (isWhile || isRep || isIf || isFor) {
+						a.add(op);
+						break;
+					}
+					isFor = true;
+					forBlock = new ArrayList<>();
+					forOp = op;
+					old.addAll(replaceWithGotos(pkg, doBlock));
+					forLabel = pkg.nameProvider.getTempName();
+					old.add(new Operation(OpType.MOV, op.type, op.range, op.args[0], op.args[2]));
+					old.add(new Operation(OpType.LABEL, forLabel));
+					a = forBlock;
+					break;
 				default:
 					a.add(op);
 			}
 		}
 		return a;
 	}
-	
+
 	public static ArrayList<Operation> replaceBlockScopes(SourcePackage pkg, ArrayList<Operation> code) {
 		ArrayList<Operation> a = new ArrayList<>();
 		return a;
