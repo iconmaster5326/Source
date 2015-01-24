c604294a4e6416bed73cdf4fda779d5044540822
Iconmaster <crazybonesj@yahoo.com>
2015-01-23 20:13:16 -0500

    Added function registry

--- src/com/iconmaster/source/link/platform/hppl/FunctionRegistry.java
+++ src/com/iconmaster/source/link/platform/hppl/FunctionRegistry.java
@@ -1,9 +1,29 @@
 package com.iconmaster.source.link.platform.hppl;
 
+import java.util.HashMap;
+
 /**
  *
  * @author iconmaster
  */
 public class FunctionRegistry {
+	public static interface CustomFunction {
+		public String assemble();
+	}
 	
+	public static CustomFunction defaultFunction;
+	
+	public static HashMap<String,CustomFunction> functions;
+	
+	public static void addFunction(String fn, CustomFunction exec) {
+		functions.put(fn, exec);
+	}
+	
+	public static boolean isFunctionCustom(String fn) {
+		return functions.containsKey(fn);
+	}
+	
+	public static String assembleFunction(String name) {
+		return isFunctionCustom(name) ? functions.get(name).assemble() : defaultFunction.assemble();
+	}
 }
