75d566935ce9defa31b4c36491bd5a74f67c3e4b
Iconmaster <crazybonesj@yahoo.com>
2015-01-24 17:02:54 -0500

    More functions done

--- src/com/iconmaster/source/link/platform/hppl/HPPLCustomFunctions.java
+++ src/com/iconmaster/source/link/platform/hppl/HPPLCustomFunctions.java
@@ -1,6 +1,7 @@
 package com.iconmaster.source.link.platform.hppl;
 
 import com.iconmaster.source.link.platform.hppl.InlinedExpression.InlineOp;
+import com.iconmaster.source.prototype.Function;
 import com.iconmaster.source.prototype.SourcePackage;
 import com.iconmaster.source.prototype.TypeDef;
 
@@ -15,6 +16,8 @@ public class HPPLCustomFunctions {
 	
 	public static void loadCore(SourcePackage pkg) {
 		pkg.getFunction("core.print").data.put("compName", "PRINT");
+		pkg.getFunction("core.ifte").data.put("compName", "IFTE");
+		
 		for (TypeDef type : LibraryCore.MATH_TYPES) {
 			pkg.getFunction("core."+type+"._add").data.put("onAssemble", (CustomFunction) (ad,op,sb) -> {
 				sb.append("(");
@@ -24,6 +27,40 @@ public class HPPLCustomFunctions {
 				sb.append(")");
 				return null;
 			});
+		}
+		
+		for (Function fn : pkg.getFunctions("core.range")) {
+			if (fn.getArguments().size()==2) {
+				fn.data.put("onAssemble", (CustomFunction) (ad,op,sb) -> {
+					String localName = HPPLNaming.getNewName();
+					sb.append("MAKELIST(");
+					sb.append(localName);
+					sb.append(",");
+					sb.append(localName);
+					sb.append(",");
+					sb.append(ad.getInline(op.op.args[2]));
+					sb.append(",");
+					sb.append(ad.getInline(op.op.args[3]));
+					sb.append(")");
+					return null;
+				});
+			} else {
+				fn.data.put("onAssemble", (CustomFunction) (ad,op,sb) -> {
+					String localName = HPPLNaming.getNewName();
+					sb.append("MAKELIST(");
+					sb.append(localName);
+					sb.append(",");
+					sb.append(localName);
+					sb.append(",");
+					sb.append(ad.getInline(op.op.args[2]));
+					sb.append(",");
+					sb.append(ad.getInline(op.op.args[3]));
+					sb.append(",");
+					sb.append(ad.getInline(op.op.args[4]));
+					sb.append(")");
+					return null;
+				});
+			}
 		}
 	}
 }
