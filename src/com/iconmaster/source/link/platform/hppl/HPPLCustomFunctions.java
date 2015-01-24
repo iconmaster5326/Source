0e50648b0e68f1fae905dd8cdf3aaae36eeb94c4
Iconmaster <crazybonesj@yahoo.com>
2015-01-24 14:44:52 -0500

    More general adds

--- src/com/iconmaster/source/link/platform/hppl/HPPLCustomFunctions.java
+++ src/com/iconmaster/source/link/platform/hppl/HPPLCustomFunctions.java
@@ -2,6 +2,7 @@ package com.iconmaster.source.link.platform.hppl;
 
 import com.iconmaster.source.link.platform.hppl.InlinedExpression.InlineOp;
 import com.iconmaster.source.prototype.SourcePackage;
+import com.iconmaster.source.prototype.TypeDef;
 
 /**
  *
@@ -13,14 +14,17 @@ public class HPPLCustomFunctions {
 	}
 	
 	public static void loadCore(SourcePackage pkg) {
-		pkg.getFunction("core.print").data.put("compName", "print");
-		pkg.getFunction("core.int32._add").data.put("onAssemble", (CustomFunction) (ad,op,sb) -> {
-			sb.append("(");
-			sb.append(ad.getInline(op.op.args[2]));
-			sb.append("+");
-			sb.append(ad.getInline(op.op.args[3]));
-			sb.append(")");
-			return null;
-		});
+		pkg.getFunction("core.print").data.put("compName", "PRINT");
+		for (TypeDef type : LibraryCore.MATH_TYPES) {
+			pkg.getFunction("core."+type+"._add").data.put("onAssemble", (CustomFunction) (ad,op,sb) -> {
+				sb.append("(");
+				sb.append(ad.getInline(op.op.args[2]));
+				sb.append("+");
+				sb.append(ad.getInline(op.op.args[3]));
+				sb.append(")");
+				return null;
+			});
+		}
+		
 	}
 }
